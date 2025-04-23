b87301f8ae2d937f24ee59372f730aeee9278369
TheOutgoingL <TheOutgoingL@github.com>
2025-04-23 17:10:31 +0800

    feat: 实现简单的like功能。可以针对char类型的数据进行模糊查询，符合提测要求。

--- src/observer/sql/expr/expression.cpp
+++ src/observer/sql/expr/expression.cpp
@@ -15,6 +15,8 @@ See the Mulan PSL v2 for more details. */
 #include "sql/expr/expression.h"
 #include "sql/expr/tuple.h"
 #include "sql/expr/arithmetic_operator.hpp"
+#include <string>
+#include <regex>
 
 using namespace std;
 
@@ -118,11 +120,38 @@ ComparisonExpr::ComparisonExpr(CompOp comp, unique_ptr<Expression> left, unique_
 
 ComparisonExpr::~ComparisonExpr() {}
 
+static void replace_all(std::string &str, const std::string &from, const std::string &to)
+{
+  if (from.empty()) {
+    return;
+  }
+  size_t pos = 0;
+  while (std::string::npos != (pos = str.find(from, pos))) {
+    str.replace(pos, from.length(), to);
+    pos += to.length();  // in case 'to' contains 'from'
+  }
+}
+static bool str_like(const Value &left, const Value &right)
+{
+  std::string raw_reg(right.data());
+  replace_all(raw_reg, "_", "[^']");
+  replace_all(raw_reg, "%", "[^']*");
+  std::regex reg(raw_reg.c_str(), std::regex_constants::ECMAScript | std::regex_constants::icase);
+  bool res = std::regex_match(left.data(), reg);
+  return res;
+}
+
 RC ComparisonExpr::compare_value(const Value &left, const Value &right, bool &result) const
 {
   RC  rc         = RC::SUCCESS;
-  int cmp_result = left.compare(right);
-  result         = false;
+  // int cmp_result = left.compare(right);
+  int cmp_result;
+  // 只有非like的操作符才需要调用compare方法
+  if (comp_ != LIKE_OP && comp_ != NOT_LIKE_OP) {
+    cmp_result = left.compare(right);
+  }
+  result = false;
+
   switch (comp_) {
     case EQUAL_TO: {
       result = (0 == cmp_result);
@@ -142,6 +171,12 @@ RC ComparisonExpr::compare_value(const Value &left, const Value &right, bool &re
     case GREAT_THAN: {
       result = (cmp_result > 0);
     } break;
+    case LIKE_OP: {
+      result = str_like(left,right);
+    }break;
+    case NOT_LIKE_OP: {
+      result = !str_like(left,right);
+    }break;
     default: {
       LOG_WARN("unsupported comparison. %d", comp_);
       rc = RC::INTERNAL;
